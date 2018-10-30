f0a0a2d2923a1146cb686193a220784b2c6b3019
Vlad <Torino111@mail.ru>
2018-10-31 01:05:43 +0300

    initial project version

---
+++ ChainShot.java
@@ -0,0 +1,24 @@
+package Edition;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ChainShot {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        System.out.print("Введите n: ");
+        Integer Str = scan.nextInt();
+        System.out.println(Tor(Str+1));
+    }
+    public static Double Tor(int lol){
+        lol--;
+        if(lol==0) return 1.;
+        return 1+1/Tor(lol) ;
+//        for (int i=0;i<=lol;i++){
+//
+//            return 0.;
+        }
+    }
+
+
