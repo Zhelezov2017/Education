0b6487b0abb6af43ba7b9ebf2de9094020498201
Vlad <Torino111@mail.ru>
2018-11-20 21:39:04 +0300

    Testing

---
+++ Test.java
@@ -0,0 +1,21 @@
+package Edition;
+
+public class Test {
+    public static void main(String[] args) {
+        int i=7;
+        i = Mark(9);
+        System.out.println(i);
+        Men gon = new Men();
+        System.out.println(gon.i);
+    }
+
+    static int Mark(int i){
+
+        return i;
+    }
+
+}
+class Men{
+    int i =3;
+
+}
